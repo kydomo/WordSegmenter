d4a81cde8a703ed294158977fa717b7d078f0678
Saquib <mohd16034@iiitd.ac.in>
2021-06-25 22:25:01 +0530

    adds wordsegment module

--- src/core/segementers/wordsegment/wordsegment.py
+++ src/core/segementers/wordsegment/wordsegment.py
@@ -1,3 +1,8 @@
-# TODO create class WordSegment
-# 
+from wordsegment import load, segment
 
+class WordSegment:
+    def __init__(self):
+        load()
+
+    def getWords(self, domain_list):
+        return segment(domain_list)
